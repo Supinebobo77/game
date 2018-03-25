e23c61d7e1df083dec1322a90e17badc70655b83
dudewad <me@laytonmiller.com>
2018-03-25 12:35:23 -0700

    Make some changes for conflict demo

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+var abc = 1000;
+
 function speak(phrase) {
   var output = phrase + ' for real!!!';
   console.log(output);
@@ -5,10 +7,16 @@ function speak(phrase) {
 
 function yell(phrase) {
   var output = phrase.toUpperCase();
+  // This makes it so we don't have to copy the logic from the speak() function!
   speak(output);
 }
 
 yell('Ronaldo is better then Messi');
 yell('Im pickle rick!!!!!!!!');
+
 yell('Tomatoes are better than Apples.');
 yell('Layton is Bowen\'s brother!');
+
+abc += 50;
+
+speak(abc);
