b04f45833848b7fc470e65120dbcb109f1a19216
Bowen Kennedy <bowenkennedy7@gmail.com>
2018-03-25 12:04:02 -0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+function speak(phrase) {
+  var output = phrase + ' for real!!!';
+  console.log(output);
+}
+
+function yell(phrase) {
+  var output = phrase.toUpperCase();
+  speak(output);
+}
+
+yell('Ronaldo is better then Messi');
+yell('Neymar is better then cavani');
