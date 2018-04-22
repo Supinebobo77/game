ce881e757254ac592a1b3bc1501ce05ee8af95b7
Bowen Kennedy <bowenkennedy7@gmail.com>
2018-04-22 12:48:03 -0700

    enemies

--- src/main.js
+++ src/main.js
@@ -6,8 +6,30 @@ var spaceship = {
   }
 };
 
+
+
+var enemies = [];
+var alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+for (var i = 0; i < 5; i++){
+  var  enemy = {letters: []};
+
+  for (var j = 0; j < 5; j ++){
+    var idx = Math.floor(Math.random() * 25);
+    enemy.letters.push(alph[idx])
+  }
+
+  enemies.push(enemy)
+
+}
+
+
+
+
 function main(){
   window.addEventListener('keydown',spaceship.fire);
 }
 main();
+
+
 
