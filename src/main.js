2ddc20fe57750258846aaaca7c1d2a30349ccf41
Bowen Kennedy <bowenkennedy7@gmail.com>
2018-04-22 12:19:35 -0700

    creat main func. and spaceship object

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
-var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var spaceship = {
+  fire: function (evt) {
+    if(evt.key === "a") {
+      console.log(evt.key);
+    }
+  }
+};
 
-for (letters = 0; letters < 23; letters++){
-
-  var letters = letters++;
-  console.log(letters);
+function main(){
+  window.addEventListener('keydown',spaceship.fire);
 }
+main();
+
