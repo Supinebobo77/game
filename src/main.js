a37bdde44dd499b4f93637fa6e7435422e79195c
Bowen Kennedy <bowenkennedy7@gmail.com>
2018-04-21 17:39:03 -0700

    another one boys

--- src/main.js
+++ src/main.js
@@ -1,56 +1,7 @@
-/*
-var abc = 1000;
+var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-function speak(phrase) {
-  var output = phrase + ' for real!!!';
-  console.log(output + '!!!!!!!!!!!!!!!!!!!!!!');
+for (letters = 0; letters < 23; letters++){
+
+  var letters = letters++;
+  console.log(letters);
 }
-
-function yell(phrase) {
-  var output = phrase.toUpperCase();
-  // This makes it so we don't have to copy the logic from the speak() function!
-  speak(output);
-}
-
-/!**
- * Comments 'n stuff
- *!/
-yell('Ronaldo is better then Messi by far');
-yell('Im pickle rick!!!!!!!!');
-yell('Hi Layton');
-yell('Yeaeaeah Baby Yeah');
-yell('apples are better then oranges');
-yell('Layton is Bowen\'s brother!');
-yell('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHAhAHAHAhahhahahAHHAhahahAHhaHHHHHHH!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
-
-abc += 50;
-
-speak(abc);
-yell('bowen has a game tomorrow -');
-var abc = ['roanldo','neymar','alexis','griezmann'];
-
-for(var i = 0; i < abc.length; i++) {
-  var output = abc[i];
-  speak(output);
-}*/
-var output = '';
-
-var dar = ['coolio!', 'awesome', 'sick bro!', 'ahhhhhh!!!!!!', 'ouch!', 'coolio!', 'awesome', 'sick bro!', 'ahhhhhh!!!!!!', 'ouch!', 'coolio!', 'awesome', 'sick bro!', 'ahhhhhh!!!!!!', 'ouch!',];
-console.log(dar);
-
-for(var i = 0; i < dar.length; i++){
-var item = dar[i];
-output = output + ' ' + item;
-}
-
-console.log(output);
-
-var final = 0;
-
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(numbers);
-
-for (var i = 0; i < numbers.length; i++) {
-  final += numbers[i];
-}
-console.log(final);
