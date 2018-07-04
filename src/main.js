7a7100ccccef739b90214ac2e99899a69be58509
Bowen Kennedy <bowenkennedy7@gmail.com>
2018-07-04 16:17:59 -0700

    Made enemies ;)

--- src/main.js
+++ src/main.js
@@ -1,80 +1,26 @@
-var spaceship = {
-  currentTarget: null,
-
-  fire: function (evt) {
-    var letter = evt.key;
-    if(!spaceship.currentTarget) {
-     spaceship.currentTarget = spaceship.acquireTarget(letter);
-    }
-
-    if (spaceship.currentTarget){
-
-      var enemy = spaceship.currentTarget;
-      if (letter === enemy.letters[0]) {
-        enemy.letters.shift();
-        console.log("HIT!", enemy.letters.join(', '));
-
-      }
-      else{
-        console.log("miss :(");
-      }
-      if (!enemy.letters.length){
-        var idx = enemies.indexOf(enemy);
-        enemies.splice(idx, 1);
-        spaceship.currentTarget = null;
-        console.log("KILLED AN ENEMY !!!:-)");
-        console.log('Remaining enemies:');
-        logEnemies();
-      }
-      if(!enemies.length) {
-        console.log('CONGRATULATIONS, YOU SAVED THE UNIVERSE!!!');
-        console.log('YOU WON!!!!!!!!! :D FREE food FOREVER!');
-      }
-    }
-  },
-
-  acquireTarget: function (key){
-    for (var i = 0; i < enemies.length; i++){
-      var enemy = enemies[i];
-      if (key === enemy.letters[0]){
-        console.log("TARGET ACQUIRED",enemy);
-        return enemy;
-      }
-    }
+function buildEnemy(word, elref) {
+  return {
+    word: word,
+    element: elref
   }
-};
-
-
-
-var enemies = [];
-var alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-for (var i = 0; i < 5; i++){
-  var  enemy = {letters: []};
-
-  for (var j = 0; j < 5; j ++){
-    var idx = Math.floor(Math.random() * 25);
-    enemy.letters.push(alph[idx])
-  }
-
-  enemies.push(enemy)
-
 }
+var enemyWordList = ['cool','fun','nice','dope','sick','fuck','flip','poop','music'];
+var hoard = [];
+var bodyEl;
 
-function logEnemies() {
-  for (var i = 0; i < enemies.length; i++) {
-    console.log(enemies[i].letters.join(','));
+function generateHoard(words) {
+  for (var i = 0; i < words.length; i ++){
+    var word = words[i];
+    var element = document.createElement('div');
+    element.classList.add('enemy');
+    var enemy = buildEnemy(word,element);
+    hoard.push(enemy);
+    bodyEl.appendChild(enemy.element);
   }
 }
 
-
-
-function main(){
-  window.addEventListener('keydown',spaceship.fire);
-  console.log('Incoming Enemise!!!');
-  logEnemies();
-}
-main();
-
-
-
+window.addEventListener('load',function () {
+  bodyEl = document.querySelector('body');
+  generateHoard(enemyWordList);
+  console.log(hoard);
+});
